a164b57491b6ace1836d654095dfe47d3aea904e
z.terehov <zaharkuli67@gmail.com>
2022-11-16 08:36:30 +0300

    Create delete contact controller method

--- ContactsAPI/ContactsAPI/Controllers/ContactsController.cs
+++ ContactsAPI/ContactsAPI/Controllers/ContactsController.cs
@@ -22,6 +22,18 @@ namespace ContactsAPI.Controllers
 			return Ok(await dbContext.Contacts.ToListAsync());
 		}
 
+		[HttpGet]
+		[Route("{id:guid}")]
+		public async Task<IActionResult> GetContact([FromRoute] Guid id)
+		{
+			var contact = await dbContext.Contacts.FindAsync(id);
+
+			if(contact == null)
+				return NotFound();
+
+			return Ok(contact);
+		}
+
 		[HttpPost]
 		public async Task<IActionResult> AddContact(AddContactRequest addContactrRequest)
 		{
@@ -53,6 +65,23 @@ namespace ContactsAPI.Controllers
 				contact.Phone = updateContactRequest.Phone;
 				contact.Email = updateContactRequest.Email;
 
+				await dbContext.SaveChangesAsync();
+
+				return Ok(contact);
+			}
+
+			return NotFound();
+		}
+
+		[HttpDelete]
+		[Route("{id:guid}")]
+		public async Task<IActionResult> DeleteContact([FromRoute] Guid id)
+		{
+			var contact = await dbContext.Contacts.FindAsync(id);
+
+			if(contact != null)
+			{
+				dbContext.Remove(contact);
 				await dbContext.SaveChangesAsync();
 
 				return Ok(contact);
